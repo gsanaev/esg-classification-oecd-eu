296f46cc79706f765bf37e6165af4997533d986e
Golib Sanaev <gsanaev80@gmail.com>
2025-11-03 18:12:49 +0100

    Initial project setup with uv and structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Classifying Corporate ESG Performance Using OECD and EU Taxonomy Indicators (2015–2025)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from esg-classification-oecd-eu!")
+
+
+if __name__ == "__main__":
+    main()
